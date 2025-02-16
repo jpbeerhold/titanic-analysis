920085122051af3f7725680e07f0b624daedcade
kubaamarczak <kubam1815@gmail.com>
2025-02-16 21:18:07 +0100

    Update description_and_visualisation.R
    data added & 2-a-i started

--- scripts/analysis/task3/description_and_visualisation.R
+++ scripts/analysis/task3/description_and_visualisation.R
@@ -1,6 +1,14 @@
+data <- read.csv("/Users/deinname/Downloads/titanic_cleaned.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+
 source("../../../task2/statistics_categorial_variables.R")
 source("../../../task2/statistics_metric_variables.R")
 source("../../../task2/visualization_categorical_variables.R")
 source("../../../task2/statistics_bivariate_categorical_variables.R")
 source("../../../task2/bivariate_statistics_metric_dichotomous.R")
+
+##Aufgabe 2-a-i
+metric_variables(data$Age)
+metric_variables(data$Fare)
+metric_variables(data$SibSp)
+metric_variables(data$Parch)
 
