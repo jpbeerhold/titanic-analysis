f0a4ab5a2ee99b2aaecec8a10a6a7e3f9a0c6a1b
kubaamarczak <kubam1815@gmail.com>
2025-02-16 22:28:08 +0100

    Update description_and_visualization.R
    structure for the task

--- scripts/analysis/task3/description_and_visualization.R
+++ scripts/analysis/task3/description_and_visualization.R
@@ -9,11 +9,17 @@ source("../../../task2/statistics_bivariate_categorical_variables.R")
 source("../../../task2/bivariate_statistics_metric_dichotomous.R")
 
 ##Aufgabe 2-a-i
+##1. Description
 metric_variables(data$Age)
+##...
 metric_variables(data$Fare)
+##...
 metric_variables(data$SibSp)
+##...
 metric_variables(data$Parch)
+##...
 
+##2. Visualization
 ##Altersverteilung
 ggplot(data, aes(x = Age)) +
   geom_histogram(binwidth = 5, fill = "blue", color = "black", alpha = 0.7) +
@@ -27,8 +33,10 @@ ggplot(data, aes(x = Fare)) +
   labs(title = "Verteilung der Ticketpreise (Fare)", x = "Fare", y = "Häufigkeit")
 
 ##Aufgabe 2-a-ii
+##1. Description
 calculate_categorical_stats(data, c("Sex", "Pclass", "Embarked", "Survived"))
 
+##2. Visualization
 ##Anzahl der Passagiere pro Klasse
 ggplot(data, aes(x = Pclass, fill = factor(Pclass))) +
   geom_bar() +
@@ -42,6 +50,9 @@ ggplot(data, aes(x = Sex, fill = Sex)) +
   labs(title = "Geschlechterverteilung", x = "Geschlecht", y = "Anzahl")
 
 ##Aufgabe 2-a-iv
+##1. Description
+
+##2. Visualization
 ##Überlebensrate nach Geschlecht
 ggplot(data, aes(x = Sex, fill = factor(Survived))) +
   geom_bar(position = "fill") +
@@ -55,6 +66,7 @@ ggplot(data, aes(x = Pclass, fill = factor(Survived))) +
   labs(title = "Überlebensrate nach Passagierklasse", x = "Passagierklasse", y = "Anteil überlebt", fill = "Überlebt")
 
 ##Aufgabe 2-a-v
+##Visualization
 visualize_categorical(data, "Embarked", "Survived")
 visualize_categorical(data, "Title", "Sex")
 visualize_categorical(data, "Title", "Embarked", "Survived")
