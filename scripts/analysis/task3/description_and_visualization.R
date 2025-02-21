9b9bfe385b8133cc00f202195e0d1d947281869b
ilkymilky <yakli0806@gmail.com>
2025-02-21 20:47:21 +0100

    Update description_and_visualization.R

--- scripts/analysis/task3/description_and_visualization.R
+++ scripts/analysis/task3/description_and_visualization.R
@@ -32,7 +32,9 @@ ggplot(data, aes(x = Age)) +
   theme_minimal() +
   labs(title = "Altersverteilung mit statistischen Kennwerten", x = "Alter", y = "Häufigkeit")
 
-##...
+# Dieses Histogramm visualisiert die Altersverteilung der Passagiere mit statistischen Kennwerten, 
+# (Mittelwert: rot gestrichelt, Median: blau gepunktet, Modus, grüner Strich.)
+
 
 ##Boxplot mit Interquartilsabstand und Spannweite
 ggplot(data, aes(y = Age)) +
@@ -45,7 +47,11 @@ ggplot(data, aes(y = Age)) +
   theme_minimal() +
   labs(title = "Boxplot der Altersverteilung", y = "Alter")
 
-##...
+# Dieser Boxplot zeigt die Verteilung des Alters, einschließlich der Quartile und der gesamten Spannweite.
+# (Blau gestrichelt, 25% Quartil, Median und 75% Quartil, Schwarz durchgezogen: Minimum und Maximum)
+
+##
+
 
 ##Dichtekurve für Schiefe & Kurtosis
 ggplot(data, aes(x = Age)) +
@@ -56,7 +62,9 @@ ggplot(data, aes(x = Age)) +
   theme_minimal() +
   labs(title = "Dichtekurve der Altersverteilung", x = "Alter", y = "Dichte")
 
-##...
+# Die Dichtekurve zeigt die geschätzte Verteilung des Alters als glatte Funktion.
+# Blaue Fläche: gibt Dichte an, wobei höhere Bereiche häufiger vorkommende Alterswerte darstellen.
+# Die rote gestrichelte Linie repräsentiert den Mittelwert, die blau gepunktete Linie den Median .
 
 ####################
 
@@ -73,7 +81,9 @@ ggplot(data, aes(x = Fare)) +
   theme_minimal() +
   labs(title = "Ticketpreisverteilung mit statistischen Kennwerten", x = "Ticketpreis", y = "Häufigkeit")
 
-##...
+# Das Histogramm zeigt die Verteilung der Ticketpreise in verschiedenen Preisklassen.
+# Die Balkenhöhen geben an, wie viele Tickets in den jeweiligen Preisbereichen liegen.
+# Die vertikalen Linien zeigt den Mittelwert (rot), den Median (blau) und den Modus (grün) für bessere Einordnung der Verteilung.
 
 ##Boxplot mit Interquartilsabstand und Spannweite
 ggplot(data, aes(y = Fare)) +
@@ -86,7 +96,9 @@ ggplot(data, aes(y = Fare)) +
   theme_minimal() +
   labs(title = "Boxplot der Ticketpreisverteilung", y = "Ticketpreis")
 
-##...
+# Der Boxplot zeigt die Verteilung der Ticketpreise und identifiziert Ausreißer.
+# Die blaue Box stellt den Interquartilsabstand (25%- bis 75%-Quartil) dar, die schwarze Linie ist die Spannweite.
+# Der Median ist als blaue gestrichelte Linie eingezeichnet, rote Punkte kennzeichnen potenzielle Ausreißer.
 
 ##Dichtekurve für Schiefe & Kurtosis
 ggplot(data, aes(x = Fare)) +
@@ -97,7 +109,9 @@ ggplot(data, aes(x = Fare)) +
   theme_minimal() +
   labs(title = "Dichtekurve der Ticketpreisverteilung", x = "Ticketpreis", y = "Dichte")
 
-##...
+# Die Dichtekurve zeigt die Wahrscheinlichkeitsverteilung der Ticketpreise.
+# Die Lage von Mittelwert und Median deutet auf eine mögliche Schiefe der Verteilung hin.
+# Eine starke Schiefe oder hohe Kurtosis deutet auf Ausreißer oder eine ungewöhnliche Preisverteilung hin.
 
 ####################
 
@@ -105,7 +119,9 @@ ggplot(data, aes(x = Fare)) +
 numeric_vars <- data %>% select(Age, Fare, SibSp, Parch)
 ggpairs(numeric_vars, title = "Paarweise Korrelationen der metrischen Variablen")
 
-##...
+# Die paarweise Korrelationsmatrix zeigt Beziehungen zwischen den numerischen Variablen.
+# Streudiagramme visualisieren die Zusammenhänge, während Korrelationskoeffizienten deren Stärke und Richtung angeben.
+# Hohe Werte die nahe ±1 sind, weisen auf starke Korrelationen hin, während Werte nah an 0 auf keinen Zusammenhang hindeuten.
 
 ########################################
 
@@ -122,7 +138,9 @@ ggplot(categorical_stats, aes(x = Category, y = Frequency, fill = Variable)) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
   scale_fill_brewer(palette = "Set2")
 
-##...
+# Das Balkendiagramm visualisiert die Häufigkeit der Kategorien (Geschlecht, Passagierklasse, Einsteigehafen, Überlebensrate).
+# Es zeigt, wie viele Passagiere in jeder Kategorie sind und unterteil dabei nach den verschiedenen Variablen.
+# Die Facetten machen eine klare Darstellung der Häufigkeiten für jede Variable möglich.
 
 #########################################################################################################################
 ##Analysen von Überlebensrate und Ticketpreis gegen andere Variablen
@@ -131,7 +149,8 @@ ggplot(categorical_stats, aes(x = Category, y = Frequency, fill = Variable)) +
 ###Überlebensrate nach Geschlecht
 visualize_categorical(data, "Sex", "Survived")
 
-##...
+# Die Funktion erstellt eine Visualisierung, die die Überlebensrate nach Geschlecht zeigt.
+# Diese Analyse ist dazu da, den Einfluss des Geschlechts auf die Überlebenswahrscheinlichkeit zu untersuchen.
 
 ##Mosaikplot Überlebensrate nach Geschlecht
 bivariate_survived_sex <- bivariate_two_categorial(data, "Survived", "Sex")
@@ -140,7 +159,8 @@ mosaicplot(bivariate_survived_sex$Kontingenztafel, col = c("lightblue", "pink"),
            main = "Mosaikplot: Überlebensrate nach Geschlecht", 
            xlab = "Geschlecht", ylab = "Überlebt")
 
-##...
+# Ein Mosaikplot stellt die Häufigkeiten der Kombinationen der beiden Variablen "Überlebensrate" und "Geschlecht" dar.
+# Der Plot zeigt visuell, wie sich die Überlebensrate je nach Geschlecht unterscheidet.
 
 ##Heatmap für Überlebensrate nach Geschlecht
 bivariate_survived_sex_df <- as.data.frame(bivariate_survived_sex$Kontingenztafel)
@@ -153,14 +173,16 @@ ggplot(bivariate_survived_sex_df, aes(x = Survived, y = Sex, fill = Frequency)) 
   labs(title = "Heatmap der Kontingenztafel: Überlebensrate nach Geschlecht",
        x = "Überlebensrate", y = "Geschlecht", fill = "Häufigkeit")
 
-##...
+# Eine Heatmap zeigt die Häufigkeiten der Überlebensraten für jedes Geschlecht.
+# Die Farben der Zellen verdeutlichen, wie stark die jeweiligen Kombinationen vertreten sind.
+# -> Je dunkler das Blau, desto höher ist die Häufigkeit.
 
 ####################
 
 ###Überlebensrate nach Passagierklasse
 visualize_categorical(data, "Pclass", "Survived")
 
-##...
+##Bei dieser Visualisierung wird die Überlebensrate nach Passagierklasse dargestellt, um signifikante Unterschiede darzustellen.
 
 ##Mosaikplot Überlebensrate nach Passagierklasse
 bivariate_survived_Pclass <- bivariate_two_categorial(data, "Survived", "Pclass")
@@ -169,7 +191,10 @@ mosaicplot(bivariate_survived_Pclass$Kontingenztafel, col = c("yellow", "orange"
            main = "Mosaikplot: Überlebensrate nach Klasse", 
            xlab = "Überlebt", ylab = "Klasse")
 
-##...
+# Mosaikplot zur Visualisierung der Überlebensrate nach Passagierklasse
+# Der Plot zeigt, wie sich die Überlebensrate (Überlebt/Nicht-Überlebt) in den verschiedenen Passagierklassen unterscheidet.
+# Die Farben repräsentieren die Häufigkeit der Überlebenden und Nicht-Überlebenden in jeder Klasse.
+
 
 ##Heatmap für Überlebensrate nach Passagierklasse
 bivariate_survived_Pclass_df <- as.data.frame(bivariate_survived_Pclass$Kontingenztafel)
@@ -179,10 +204,13 @@ ggplot(bivariate_survived_Pclass_df, aes(x = Survived, y = Pclass, fill = Freque
   geom_tile(color = "white") +
   scale_fill_gradient(low = "white", high = "blue") +
   theme_minimal() +
-  labs(title = "Heatmap der Kontingenztafel: Überlebensrate nach Geschlecht",
+  labs(title = "Heatmap der Kontingenztafel: Überlebensrate nach Passagierklasse",
        x = "Überlebensrate", y = "Geschlecht", fill = "Häufigkeit")
 
-##...
+# Heatmap zur Visualisierung der Überlebensrate nach Passagierklasse
+# Die Heatmap stellt die Häufigkeiten der Kombinationen von Überlebensstatus und Passagierklasse  dar.
+# Farbverläufe von weiß bis blau zeigen die Frequenz der jeweiligen Kombination, wobei höhere Frequenzen intensiver in Blau dargestellt werden.
+
 
 ####################
 
@@ -192,28 +220,41 @@ ggplot(data, aes(x = factor(Survived), y = Age, fill = factor(Survived))) +
   theme_minimal() +
   labs(title = "Überlebensrate nach Alter", x = "Überlebt", y = "Alter", fill = "Überlebt")
 
-##...
+# Boxplot zur Visualisierung der Überlebensrate im Vergleich zum Alter
+# Der Boxplot zeigt, wie sich das Alter (y-Achse) für Überlebende (1) und Nicht-Überlebende (0) (x-Achse) unterscheidet.
+# Überlebende und Nicht-Überlebende werden durch unterschiedliche Farben (fill) hervorgehoben.
+# Der Boxplot macht es möglich, die Verteilung und Streuung des Alters in beiden Gruppen darzustellen.
+
 
 ####################
 
 ###Überlebensrate nach Anzahl der Geschwister/Ehepartner
 visualize_categorical(data, "SibSp", "Survived")
 
-##...
+# Visualisierung der Überlebensrate in Bezug auf die Anzahl der Geschwister/Ehepartner an Bord
+# Der Funktionsaufruf visualisiert die Häufigkeit der Überlebensraten für verschiedene Werte von "Anzahl der Geschwister/Ehepartner".
+# Es wird überprüft, wie sich die Überlebensrate im Verhältnis zur Anzahl der Geschwister/Ehe
+
 
 ####################
 
 ###Überlebensrate nach Anzahl der Eltern/Kinder
 visualize_categorical(data, "Parch", "Survived")
 
-##...
+# Visualisierung der Überlebensrate in Bezug auf die Anzahl der Eltern/Kinder an Bord
+# Der Funktionsaufruf visualisiert die Häufigkeit der Überlebensraten für verschiedene Werte von "Anzahl der Eltern/Kinder".
+# Ist dazu da um zu untersuchen,wie sich die Überlebensrate je nach Anzahl der Eltern/Kinder unterscheidet.
+
 
 ####################
 
 ###Überlebensrate nach Einsteigehafen
 visualize_categorical(data, "Embarked", "Survived")
 
-##...
+# Visualisierung der Überlebensrate in Bezug auf den Einsteigehafen
+# Der Funktionsaufruf visualisiert die Häufigkeit der Überlebensraten für verschiedene Werte von "Einsteigehafen".
+# Es wird untersucht, ob der Einsteigehafen einen Einfluss auf die Überlebensrate hatte.
+
 
 ##Mosaikplot Überlebensrate nach Einsteigehafen
 bivariate_survived_embarked <- bivariate_two_categorial(data, "Survived", "Embarked")
@@ -222,7 +263,10 @@ mosaicplot(bivariate_survived_embarked$Kontingenztafel, col = c("yellow", "orang
            main = "Mosaikplot: Überlebensrate nach Einsteigehafen", 
            xlab = "Überlebt", ylab = "Einsteigehafen")
 
-##...
+# Mosaikplot zur Visualisierung der Überlebensrate nach Einsteigehafen
+# Der Mosaikplot zeigt die Häufigkeit der Überlebensraten für verschiedene Einsteigehäfen.
+# Es wird untersucht, ob die Überlebensrate je nach Einsteigehafen unterschiedlich ist.
+
 
 ##Heatmap für Überlebensrate nach Einsteigehafen
 bivariate_survived_embarked_df <- as.data.frame(bivariate_survived_Pclass$Kontingenztafel)
@@ -235,21 +279,30 @@ ggplot(bivariate_survived_embarked_df, aes(x = Survived, y = Embarked, fill = Fr
   labs(title = "Heatmap der Kontingenztafel: Überlebensrate nach Einsteigehafen",
        x = "Überlebensrate", y = "Einsteigehafen", fill = "Häufigkeit")
 
-##...
+### Heatmap zur Visualisierung der Überlebensrate nach Einsteigehafen
+# Die Heatmap zeigt die Häufigkeit der Überlebensraten in Bezug auf den Einsteigehafen.
+# Diese gibt Auskunft darüber, ob der Einsteigehafen einen Einfluss auf die Überlebenswahrscheinlichkeit hatte.
+
 
 ####################
 
 ###Überlebensrate nach Passagierklasse und Geschlecht
 visualize_categorical(data, "Survived", "Pclass", "Sex")
 
-##...
+# Visualisierung der Überlebensrate nach Passagierklasse und Geschlecht
+# Diese Funktion zeigt die Verteilung der Überlebensraten je nach Passagierklasse und Geschlecht.
+# Es wird überprüft, ob das Überleben mit der Passagierklasse und dem Geschlecht in Verbindung steht.
+
 
 ####################
 
 ###Überlebensrate nach Passagierklasse und Einsteigehafen
 visualize_categorical(data, "Survived", "Pclass", "Embarked")
 
-##...
+# Visualisierung der Überlebensrate nach Passagierklasse und Einsteigehafen
+# Diese Funktion zeigt, wie sich die Überlebensraten für verschiedene Kombinationen von Passagierklasse und Einsteigehafen verteilen.
+# Es wird überprüft, ob es Unterschiede in der Überlebensrate zwischen den verschiedenen Kombinationen gibt.
+
 
 ########################################
 
@@ -261,7 +314,10 @@ ggplot(data, aes(x = factor(Survived), y = Fare, fill = factor(Survived))) +
   scale_y_log10() +
   labs(title = "Ticketpreis nach Überlebensrate", x = "Überlebt", y = "Ticketpreis (log)", fill = "Überlebt")
 
-##...
+# Boxplot der Ticketpreisverteilung nach Überlebensrate
+# Der Ticketpreis wird auf der y-Achse dargestellt, wobei die Überlebensrate als Gruppierung (x-Achse) verwendet wird.
+# Es wird eine logarithmische Skala für den Ticketpreis angewendet, um die Preisverteilung besser darzustellen.
+
 
 ####################
 
@@ -272,7 +328,10 @@ ggplot(data, aes(x = factor(Pclass), y = Fare, fill = factor(Pclass))) +
   scale_y_log10() +
   labs(title = "Ticketpreis nach Passagierklasse", x = "Passagierklasse", y = "Ticketpreis (log)", fill = "Klasse")
 
-##...
+# Boxplot der Ticketpreisverteilung nach Passagierklasse
+# Der Ticketpreis wird auf der y-Achse dargestellt, während die Passagierklasse als Gruppierung (x-Achse) verwendet wird.
+# Eine logarithmische Skala wird angewendet, um die Preisverteilung besser zu visualisieren.
+
 
 ####################
 
@@ -283,7 +342,10 @@ ggplot(data, aes(x = factor(Embarked), y = Fare, fill = factor(Embarked))) +
   scale_y_log10() +
   labs(title = "Ticketpreis nach Einsteigehafen", x = "Hafen", y = "Ticketpreis (log)", fill = "Hafen")
 
-##...
+# Boxplot der Ticketpreisverteilung nach Einsteigehafen
+# Der Ticketpreis wird auf der y-Achse dargestellt, während die Einsteigehafen als Gruppierung (x-Achse) verwendet wird.
+# Eine logarithmische Skala wird angewendet, um die Preisverteilung besser zu visualisieren.
+
 
 ###Ticketpreis nach Familiengröße
 data$FamilySize <- data$SibSp + data$Parch
@@ -292,7 +354,10 @@ ggplot(data, aes(x = factor(FamilySize), fill = factor(Survived))) +
   theme_minimal() +
   labs(title = "Überlebensrate nach Familiengröße", x = "Familiengröße", y = "Relative Häufigkeit")
 
-##...
+# Balkendiagramm der Überlebensrate nach Familiengröße
+# Die Familiengröße wird auf der x-Achse angezeig.
+# Die relative Häufigkeit der Überlebensraten wird für jede Familiengröße als gestapeltes Balkendiagramm dargestellt wird.
+
 
 ########################################
 ##Weitere nützliche Analysen:
@@ -304,7 +369,10 @@ ggplot(data, aes(x = Age, y = Fare, color = factor(Survived))) +
   scale_y_log10() +
   labs(title = "Scatterplot: Alter nach Ticketpreis und Überlebensrate", x = "Alter", y = "Ticketpreis (log)", color = "Überlebt")
 
-##...
+# Scatterplot, der das Alter und den Ticketpreis mit der Überlebensrate kombiniert
+# Die Punkte im Diagramm repräsentieren die Passagiere. Der Ticketpreis wird auf der y-Achse und das Alter auf der x-Achse angezeigt.
+# Die Farbe der Punkte zeigt die Überlebensrate, wobei ein transparenter Punkt (alpha = 0.5) verwendet wird, um Überlappungen zu minimieren.
+
 
 ####################
 
@@ -316,7 +384,10 @@ ggplot(data, aes(x = Age, fill = factor(Survived))) +
   labs(title = "Altersverteilung nach Geschlecht und Überlebensrate",
        x = "Alter", y = "Häufigkeit", fill = "Überlebt")
 
-##...
+# Das Histogramm zeigt die Verteilung des Alters der Passagiere, wobei die Überlebensrate durch unterschiedliche Farben dargestellt wird.
+# Die Facetten unterteilen die Darstellung nach Geschlecht, sodass die Verteilung für Männer und Frauen separat angezeigt wird.
+# Die Transparenz der Balken (alpha = 0.5) wird verwendet, um Überlappungen der Balken zwischen den Überlebensgruppen zu reduzieren.
+
 
 ####################
 
@@ -328,7 +399,10 @@ mosaicplot(bivariate_sex_embarked$Kontingenztafel, col = c("yellow", "orange", "
            main = "Mosaikplot: Geschlecht vs. Einsteigehafen", 
            xlab = "Geschlecht", ylab = "Einsteigehafen")
 
-##...
+# Der Mosaikplot visualisiert die Häufigkeiten der Geschlechter in Bezug auf den Einsteigehafen.
+# Jede Zelle im Plot stellt eine Kombination von Geschlecht und Einsteigehafen dar.
+# Farben werden verwendet, um die Häufigkeit der jeweiligen Kombinationen zu unterscheiden, wobei rot für eine höhere Häufigkeit steht.
+
 
 ##Proportionen von Männern und Frauen im Bezug auf Einsteigehafen
 ggplot(data, aes(x = Embarked, fill = Sex)) +
@@ -338,7 +412,10 @@ ggplot(data, aes(x = Embarked, fill = Sex)) +
        x = "Einsteigehafen", y = "Proportion") +
   scale_fill_manual(values = c("male" = "blue", "female" = "pink"))
 
-##...
+# Das Balkendiagramm zeigt die prozentualen Anteile von Männern und Frauen in den verschiedenen Einsteigehäfen.
+# Mit der "position = 'fill'" Option wird jeder Balken auf 100% normalisiert, um die Verhältnisse innerhalb der Einsteigehäfen zu verdeutlichen.
+# Die Farben blau und pink sind für Männer bzw. Frauen gewählt, um die Kategorien zu unterscheiden.
+
 
 ##Heatmap für Geschlecht nach Einsteigehafen
 bivariate_sex_embarked_df <- as.data.frame(bivariate_sex_embarked$Kontingenztafel)
@@ -351,38 +428,40 @@ ggplot(bivariate_sex_embarked_df, aes(x = Embarked, y = Sex, fill = Frequency)) 
   labs(title = "Heatmap der Kontingenztafel: Geschlecht vs. Einsteigehafen",
        x = "Einsteigehafen", y = "Geschlecht", fill = "Häufigkeit")
 
-##...
+# Die Heatmap zeigt die Häufigkeit der Passagiere in Bezug auf ihr Geschlecht und den Einsteigehafen. 
+# Die Farbskala geht von Weiß (niedrige Häufigkeit) bis Blau (hohe Häufigkeit).
+#Anhand der Visualisierung können wir sehen, wie sich die Verteilung von Männern und Frauen je nach Einsteigehafen unterscheidet.
 
 ####################
 
 ##Zusammenhang zwischen Titel, Einsteigehafen und Überlebensrate
 visualize_categorical(data, "Title", "Embarked", "Survived")
-##...
+# Zeigt, wie Titel, Einsteigehafen und Überlebensrate miteinander in Beziehung stehen.
 
 ####################
 
 ##Zusammenhang zwischen Einsteigehafen, Titel, Geschlecht und Überlebensrate
 visualize_categorical(data, "Embarked", "Title", "Sex", "Survived")
-##...
+# Zeigt, wie Titel, Einsteigehafen Geschlecht und Überlebensrate in Beziehung zueinander stehen.
 
 ####################
 
 ##Korrelationen zwischen metrischen und dichotomen Variablen
 ##Korrelation zwischen Alter und Geschlecht
 punktbiseriale_korrelation(data$Age, data$Sex)
-##...
+# Zeigt, wie Alter und Geschlecht in Beziehung zu einander stehen.
 
 ##Korrelation zwischen Alter und Überlebensrate
 punktbiseriale_korrelation(data$Age, data$Survived)
-##...
+# Zeigt, wie Alter und Überlebensrate in Beziehung zu einander stehen.
 
 ##Korrelation zwischen Ticketpreis und Geschlecht
 punktbiseriale_korrelation(data$Fare, data$Sex)
-##...
+# Zeigt, wie Ticketpreis und Geschlecht in Beziehung zu einander stehen.
 
 ##Korrelation zwischen Ticketpreis und Überlebensrate
 punktbiseriale_korrelation(data$Fare, data$Survived)
-##...
+# Zeigt, wie Ticketpreis und Überlebensrate in Beziehung zu einander stehen.
 
 
 
